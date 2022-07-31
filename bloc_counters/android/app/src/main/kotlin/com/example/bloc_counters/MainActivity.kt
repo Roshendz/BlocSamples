6d93a7658ce0e6298c6b11349b31fa4c634d1d10
Roshendz <zoysa3@me.com>
2022-07-31 12:54:05 +0200

    Counter app for demonstrate simple BloC concepts

---
+++ bloc_counters/android/app/src/main/kotlin/com/example/bloc_counters/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bloc_counters
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ bloc_counters/lib/cubit/counter_state.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+part of 'counter_cubit.dart';
+
+class CounterState {
+  int counterValue;
+  bool wasIncrented;
+  CounterState({
+    required this.counterValue,
+    required this.wasIncrented,
+  });
+}

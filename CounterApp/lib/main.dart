d20cd95ef8c563a11596e507b0b49aea18382ae5
Roshendz <zoysa3@me.com>
2022-08-13 13:44:31 +0200

    Add counter & theme cubits

--- CounterApp/lib/main.dart
+++ CounterApp/lib/main.dart
@@ -1,26 +1,102 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sizer/sizer.dart';
 
 import 'core/constants/strings.dart';
 import 'core/themes/app_theme.dart';
+import 'logic/cubit/counter_cubit.dart';
+import 'logic/cubit/theme_cubit.dart';
 import 'logic/debug/app_bloc_observer.dart';
 import 'presentation/router/app_router.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = AppBlocObserver();
-  runApp(App());
+  final storage = await HydratedStorage.build(
+    storageDirectory: await getApplicationDocumentsDirectory(),
+  );
+
+  HydratedBlocOverrides.runZoned(
+    () => runApp(
+      DevicePreview(
+        enabled: false,
+        builder: (context) => App(),
+      ),
+    ),
+    storage: storage,
+  );
 }
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: Strings.appTitle,
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-      debugShowCheckedModeBanner: false,
-      initialRoute: AppRouter.home,
-      onGenerateRoute: AppRouter.onGenerateRoute,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<ThemeCubit>(
+          create: (context) => ThemeCubit(),
+        ),
+        BlocProvider<CounterCubit>(
+          create: (context) => CounterCubit(),
+        ),
+      ],
+      child: const CounterApp(),
+    );
+  }
+}
+
+class CounterApp extends StatefulWidget {
+  const CounterApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _CounterAppState createState() => _CounterAppState();
+}
+
+class _CounterAppState extends State<CounterApp> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    WidgetsBinding.instance.addObserver(this);
+    super.initState();
+  }
+
+  @override
+  void didChangePlatformBrightness() {
+    context.read<ThemeCubit>().updateAppTheme();
+    super.didChangePlatformBrightness();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return OrientationBuilder(
+          builder: (context, orientation) {
+            return Sizer(builder: (context, constraints, orientation) {
+              return MaterialApp(
+                builder: DevicePreview.appBuilder,
+                title: Strings.appTitle,
+                theme: AppTheme.lightTheme,
+                darkTheme: AppTheme.darkTheme,
+                themeMode: context.select(
+                    (ThemeCubit themeCubit) => themeCubit.state.themeMode),
+                debugShowCheckedModeBanner: false,
+                initialRoute: AppRouter.counter,
+                onGenerateRoute: AppRouter.onGenerateRoute,
+              );
+            });
+          },
+        );
+      },
     );
   }
 }
--- CounterApp/lib/presentation/router/app_router.dart
+++ CounterApp/lib/presentation/router/app_router.dart
@@ -1,21 +1,18 @@
 import 'package:flutter/material.dart';
 
-import '../../core/constants/strings.dart';
 import '../../core/exceptions/route_exception.dart';
-import '../screens/home_screen/home_screen.dart';
+import '../screens/counter_screen/counter_screen.dart';
 
 class AppRouter {
-  static const String home = '/';
+  static const String counter = 'counter ';
 
   const AppRouter._();
 
   static Route<dynamic> onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
-      case home:
+      case counter:
         return MaterialPageRoute(
-          builder: (_) => const HomeScreen(
-            title: Strings.homeScreenTitle,
-          ),
+          builder: (_) => const CounterScreen(),
         );
       default:
         throw const RouteException('Route not found!');
